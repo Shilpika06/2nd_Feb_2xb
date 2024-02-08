03795441f708ba943d88c226cbd7d2b2c7f50d2e
Shilpika <shilpika06@gmailcom>
2024-02-07 21:01:34 -0800

    "This is task one to display user details

---
+++ task2.py
@@ -0,0 +1,27 @@
+#Printing tabel of 2 using print command
+print("TABEL OF 2 ")
+print("2*1 = ", 2*1)
+print("2*2 = ", 2*2)
+print("2*3 = ", 2*3)
+print("2*4 = ", 2*4)
+print("2*5 = ", 2*5)
+print("2*6 = ", 2*6)
+print("2*7 = ", 2*7)
+print("2*8 = ", 2*8)
+print("2*9 = ", 2*9)
+print("2*10 = ", 2*10)
+
+
+print("Which number table you want? ")
+i = int(input())
+print(i,"* 1 = ", i*1)
+print(i,"* 2 = ", i*2)
+print(i,"* 3 = ", i*3)
+print(i,"* 4 = ", i*4)
+print(i,"* 5 = ", i*5)
+print(i,"* 6 = ", i*6)
+print(i,"* 8 = ", i*8)
+print(i,"* 9 = ", i*9)
+print(i,"* 10 = ", i*10)
+
+
